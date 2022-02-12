1ed02f02be17fb52243a9330f19c368833d09134
armejbha <abdurrahim181878@gmil.com>
2022-02-12 13:34:50 +0600

    bank-site-complete

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('submit-button').addEventListener('click', function () {
+    const emailInput = document.getElementById('email-input');
+    const emailPart = emailInput.value;
+    const
+})
